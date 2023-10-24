c89179de22f725f9821f22fbc944449dc2d4764a
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-24 11:35:45 -0300

    feat: add controller

--- src/main/java/br/com/rodrigoscoutinho/messagingkafka/MessagingKafkaApplication.java
+++ src/main/java/br/com/rodrigoscoutinho/messagingkafka/MessagingKafkaApplication.java
@@ -2,6 +2,10 @@ package br.com.rodrigoscoutinho.messagingkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MessagingKafkaApplication {
@@ -10,4 +14,22 @@ public class MessagingKafkaApplication {
 		SpringApplication.run(MessagingKafkaApplication.class, args);
 	}
 
+}
+
+
+@RestController
+@RequestMapping("/kafka")
+
+class HelloController {
+	private HelloProducer service;
+
+	public HelloController(HelloProducer service) {
+		this.service = service;
+	}
+
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name){
+		service.senMessage("Hello " + name);
+		return "OK";
+  }
 }
